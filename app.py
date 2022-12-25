afe084901142af79c333ad93d836f58ddb44c9c6
yaaa3ser <yasserissa1411@gmail.com>
2022-12-26 00:41:47 +0200

    added design to gui and edited readme

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 from tkinter import *
 from worldcup.distributer import *
-root = Tk()
-bg = PhotoImage(file = "./qatar-2022-world-cup-logo-black-color-print-png-11668697201tgpljavrk0.png")
-root.title("World Cup GUI simulation_Group 6")
-root.geometry('1024x500')
-label1 = Label( root, image = bg)
-label1.place(x = 0, y = 0)
+import sys
+top = Tk()
 
-addTeamLable = Label(text="Add Team", bg = 'white', fg = 'black')
+top.title("World Cup simulation_Group 6")
+top.geometry('1024x500')
+top.configure(bg = '#0A2647')
+
+addTeamLable = Label(text="Add Team",bg = '#0A2647',fg ='white', font=('Helvetica bold', 24))
 addTeamLable.pack()
-teamEntry = Entry(width=50, bg = 'white', fg = 'black')
+teamEntry = Entry(width=50, font=('Helvetica bold', 24))
 teamEntry.pack()
-x = Label(text="", bg = '#DECEC8', fg = 'black')
-x.pack()
-strengthLable = Label(text="Strength", bg = 'white', fg = 'black')
+
+strengthLable = Label(text="Strength",bg = '#0A2647',fg ='white', font=('Helvetica bold', 24))
 strengthLable.pack()
-strength = Entry(width=50, bg = 'white', fg = 'black')
+strength = Entry(width=50, font=('Helvetica bold', 24))
 strength.pack()
-response = Label(text='', bg = '#DECEC8')
+response = Label(text='' , bg= '#0A2647' , fg = 'white',font=('Helvetica bold', 24))
 response.pack()
 
 complete = False
@@ -32,35 +31,27 @@ def add_custom_team():
         teams.append(str(teamEntry.get()))
         level = {"team" : str(teamEntry.get()) , "strength" : int(strength.get())}
         classification.append(level)
-        response.config(text="created successfuly", bg = 'white', fg = 'black')
-
+        response.config(text="Created successfuly")
     else :
-        response.config(text="you created more than 32 team")
-    
-
+        response.config(text="You created more than 32 team")
     return
+
 tables , results = [0]*8 , [0]*8
-tableLable = Label( bg = '#DECEC8')
-resultLable = Label( bg = '#DECEC8')
+tableLable = Label(fg = 'white' , bg = '#0A2647')
+resultLable = Label(fg = 'white' , bg = '#0A2647')
 tableLable.pack()
 resultLable.pack()
-
-
 
 def random_complete():
     global tables ,results, classification,complete,counter,generate,addTeamB ,nextb ,backb,team,strength
     
     complete = True
-    print(classification)
     global counterg
     for i in range(counterg,32):
-        teams.append("team_"+str(i) if i >= 10 else 'team_0'+str(i) )
-        team = {"team" : 'team_'+str(i) if i >= 10 else 'team_0'+str(i) , "strength" : randint(1,4)}
+        teams.append("Team_"+str(i) if i >= 10 else 'team_0'+str(i) )
+        team = {"team" : 'Team_'+str(i) if i >= 10 else 'Team_0'+str(i) , "strength" : randint(1,4)}
         classification.append(team)
         
-    print(classification)
-
-
     classification = sorted(classification, key=itemgetter('strength'),reverse=True) 
 
     levels = [classification[0:8], classification[8:16] ,classification[16:24] ,classification[24:32]]
@@ -82,8 +73,7 @@ def random_complete():
         qualified.append(temp[0])
         qualified.append(temp[1])
     
-    
-    tableLable.config(text=str('Group : '+ str((counter+1)%8 if (counter+1) % 8 != 0 else '8') +'\n'+'----------\n')+str(print_table(tables[counter%8])))
+    tableLable.config(text=str(print_table(tables[counter%8])))
     resultLable.config(text=str(print_result(results[counter%8])))
     generate.destroy()
     addTeamB.destroy()
@@ -91,17 +81,16 @@ def random_complete():
     strength.destroy()
     addTeamLable.destroy()
     strengthLable.destroy()
-    nextb.place(x=950, y=250)
+    nextb.place(x=910, y=250)
     backb.place(x=20, y = 250)
-    nextStageB = Button(text='next stage' , command=next_stage, bg = 'white', fg = 'black')
-    nextStageB.place(x=490 , y=410)
-    response.config(text='')
-
+    nextStageB.place(x=425 , y=430)
+    response.config(text=str('Group : '+ str((counter+1)%8 if (counter+1) % 8 != 0 else '8') +'\n'+'----------\n'))
 
 def next():
     global counter
     counter+=1
-    tableLable.config(text=str('Group : '+ str((counter+1)%8 if (counter+1) % 8 != 0 else '8') +'\n'+'----------\n')+str(print_table(tables[counter%8])))
+    response.config(text='Group : '+ str((counter+1)%8 if (counter+1) % 8 != 0 else '8') +'\n'+'----------\n', font=('Helvetica bold', 24))
+    tableLable.config(text=str(print_table(tables[counter%8])))
     resultLable.config(text=str(print_result(results[counter%8])))
         
 def back():
@@ -109,28 +98,34 @@ def back():
     counter-=1
     tableLable.config(text=str('Group : '+ str((counter+1)%8 if (counter+1) % 8 != 0 else 8) +'\n'+'----------\n')+str(print_table(tables[counter%8])))
     resultLable.config(text=str(print_result(results[counter%8])))
-        
+
+def exit():
+    sys.exit(0)
+    
+
 def next_stage():
     global qualified , nextStageB
     if len(qualified) == 1:
-        tableLable.config(text='The CHAMPION is : '+str(qualified[0]))
-        response.config(text='---------\n| Winner |\n----------')
+        tableLable.config(text='The CHAMPION is : '+str(qualified[0]), font=('Helvetica bold', 48))
+        response.config(text='---------\n| Winner |\n----------', font=('Helvetica bold', 24))
+        exitb = Button(text='EXIT' , command=exit , font=('Helvetica bold', 24))
+        exitb.place(x=460 , y=430)
         nextStageB.destroy()
         return
+    
     if len(qualified) == 8:
-        response.config(text='----------------\n| quarter final |\n-----------------')
+        response.config(text='----------------\n| Quarter final |\n-----------------', font=('Helvetica bold', 24))
     elif len(qualified) == 4:
-        response.config(text='------------\n| semi final |\n-------------')
+        response.config(text='------------\n| Semi final |\n-------------', font=('Helvetica bold', 24))
     elif len(qualified) == 2:
-        response.config(text='--------\n| final |\n--------')
+        response.config(text='--------\n| Final |\n--------', font=('Helvetica bold', 24))
     else :
-        response.config(text='-------------\n| round 16 |\n--------------')
+        response.config(text='-------------\n| Round 16 |\n--------------', font=('Helvetica bold', 24))
     
     stage = KnockOut(len(qualified) , qualified)
     stage.create_matches()
     qualified = stage.qualified()
     tableLable.config(text=str(stage.print_games()))
-        
         
     if resultLable:
         resultLable.destroy()
@@ -138,24 +133,19 @@ def next_stage():
         nextb.destroy()
     if backb:
         backb.destroy()
-    
+nextStageB = Button(text='Next stage' , command=next_stage, font=('Helvetica bold', 24))
 
+nextb = Button(text="Next",command=next, bg = 'white',font=('Helvetica bold', 24))
+backb = Button(text="Back",command=back,bg = 'white', font=('Helvetica bold', 24)) 
 
-nextStageB = Button(text='next stage' , command=next_stage , bg = 'white', fg = 'black')
-
-    
-nextb = Button(text="next",command=next, bg = 'white', fg = 'black')
-backb = Button(text="back",command=back, bg = 'white', fg = 'black') 
-
-
-addTeamB = Button(text="Add team",command=add_custom_team,width=25, bg = 'white', fg = 'black' )
+addTeamB = Button(text="Add team",command=add_custom_team,width=25, bg = 'white',font=('Helvetica bold', 24))
 addTeamB.pack()
-space = Label(bg = '#DECEC8')
+space = Label(bg = '#0A2647')
 space.pack()
-generate = Button(text="Generate and view",command=random_complete,width=30, bg = 'white', fg = 'black')
+generate = Button(text="Generate and view",command=random_complete,width=30, bg = 'white',font=('Helvetica bold', 24))
 generate.pack()
 
 
 
 
-mainloop()
+top.mainloop()
--- worldcup/distributer.py
+++ worldcup/distributer.py
@@ -33,7 +33,6 @@ class Group:
         if eval(score1)> eval(score2):
             for team in self.table:
                 if team['team_name'] == team1['team']:
-                    print(team1)
                     team['score']+=3
             result = {
                 "team1":team1['team'],
@@ -59,7 +58,7 @@ class Group:
                 "team1":team1['team'],
                 "team2":team2['team'],
                 "score" : score1 + ' - ' + score2,
-                "winner":'  tie        '
+                "winner":'  Tie        '
             }
         self.result.append(result)
 
@@ -79,17 +78,17 @@ class Group:
 
 def print_table(table):
     tables=""
-    tables+='   team name     |       points         \n'
+    tables+='   Team name    |       points         \n'
     tables+='-----------------------------------\n'
 
     for i in range(4):
-        tables+= '     ' + str(table[i]['team_name']) + '      |' +'     ' + str(table[i]['score']) + '     ' + ('qulified\n' if i < 2 else '             \n')
+        tables+= '     ' + str(table[i]['team_name']) + '      |' +'     ' + str(table[i]['score']) + '     ' + ('Qulified\n' if i < 2 else '             \n')
     tables += '========================================\n'    
     return tables
 
 def print_result(result):
     results = ""
-    results+= '    team 1        VS     team 2       |     score     |     winner     \n'
+    results+= '    Team 1        VS     Team 2       |     score     |     winner     \n'
     results+= '----------------------------------------------------------------------\n'
 
     for i in range(6):
@@ -127,7 +126,7 @@ class KnockOut:
     
     def print_games(self):
         results = ""
-        results+= '    match    |     team 1       VS     team 2       |      score     |     winner         \n'
+        results+= '    match    |     Team 1       VS     Team 2       |      score     |     winner         \n'
         results+= '-----------------------------------------------------------------------\n'
 
         for game in self.games:
